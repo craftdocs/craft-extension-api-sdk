76599f6d82c3174a105533a882ff8a3d397f9678
Tamas Flamich <tom@luki.io>
2021-12-16 14:35:19 +0100

    upgraded craft-extension-api to 0.0.4

--- src/browser_module/apis/block_factory.ts
+++ src/browser_module/apis/block_factory.ts
@@ -2,16 +2,27 @@ import {
   BlockFactory,
   CraftCodeBlockConfig,
   CraftCodeBlockInsert,
+  CraftDrawingBlockConfig,
+  CraftDrawingBlockInsert,
+  CraftFileBlockConfig,
+  CraftFileBlockInsert,
   CraftHorizontalLineBlockConfig,
   CraftHorizontalLineBlockInsert,
+  CraftImageBlockConfig,
+  CraftImageBlockInsert,
+  CraftTableBlockConfig,
+  CraftTableBlockInsert,
   CraftTextBlockConfig,
   CraftTextBlockInsert,
+  CraftUrlBlockConfig,
+  CraftUrlBlockInsert,
+  CraftVideoBlockConfig,
+  CraftVideoBlockInsert,
   ListStyle,
   ListStyleType } 
 from "@craftdocs/craft-extension-api"
 
 export class BlockFactoryMock implements BlockFactory {
-
   public textBlock(block: CraftTextBlockConfig): CraftTextBlockInsert {
     return {
       type: "textBlock",
@@ -20,17 +31,11 @@ export class BlockFactoryMock implements BlockFactory {
   }
 
   public codeBlock(block: CraftCodeBlockConfig): CraftCodeBlockInsert {
-    return {
-      type: "codeBlock",
-      ...block
-    }
+    return create("codeBlock", block)
   }
 
   public horizontalLineBlock(block: CraftHorizontalLineBlockConfig): CraftHorizontalLineBlockInsert {
-    return {
-      type: "horizontalLineBlock",
-      ...block
-    }
+    return create("horizontalLineBlock", block)
   }
 
   public defaultListStyle(type: ListStyleType): ListStyle {
@@ -42,4 +47,32 @@ export class BlockFactoryMock implements BlockFactory {
     }
     return { type }
   }
+
+  public urlBlock(block: CraftUrlBlockConfig): CraftUrlBlockInsert {
+    return create("urlBlock", block)
+  }
+
+  public tableBlock(block: CraftTableBlockConfig): CraftTableBlockInsert {
+    return create("tableBlock", block)
+  }
+
+  public imageBlock(block: CraftImageBlockConfig): CraftImageBlockInsert {
+    return create("imageBlock", block)
+  }
+
+  public videoBlock(block: CraftVideoBlockConfig): CraftVideoBlockInsert {
+    return create("videoBlock", block)
+  }
+
+  public fileBlock(block: CraftFileBlockConfig): CraftFileBlockInsert {
+    return create("fileBlock", block)
+  }
+
+  public drawingBlock(block: CraftDrawingBlockConfig): CraftDrawingBlockInsert {
+    return create("drawingBlock", block)
+  }
+}
+
+function create<TType extends string, TDetails extends {}>(type: TType, details: TDetails): TDetails & { type: TType } {
+  return { type, ... details };
 }
